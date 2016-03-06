cfd08e15b414e11b11bc10eb726d692285b856ef
David Hara <hodavidhara@gmail.com>
2016-03-06 02:26:28 -0800

    Add amazing caching middleware

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ var apiRouter = require('./server/routes/api');
 var webpackDevMiddleware = require('koa-webpack-dev-middleware');
 var webpack = require('webpack');
 var webpackConfig = require('./webpack.config.js');
+var cache = require('koa-rest-cache');
 
 var app = koa();
 
 var compiler = webpack(webpackConfig);
 app.use(webpackDevMiddleware(compiler, {
-    stats: {
-        colors: true
-    }
+  stats: {
+    colors: true
+  }
+}));
+
+app.use(cache({
+  pattern: '/api/**/*'
 }));
 
 app.use(siteRouter);
 app.use(apiRouter.routes());
 app.use(apiRouter.allowedMethods());
 var server = app.listen(3000, function() {
-    console.log('Successfully started up');
+  console.log('Successfully started up');
 });

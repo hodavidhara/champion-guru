b70a90a904b4dd100034894374b92a3ac0a51072
David Hara <hodavidhara@gmail.com>
2016-03-07 23:18:17 -0800

    Aggregating data fetching into the api call so we don't have to do funky things on the front end

--- client/js/components/mastery/MasteryCard.js
+++ client/js/components/mastery/MasteryCard.js
@@ -1,39 +1,16 @@
 import React from "react";
-import ChampionData from "../data/ChampionData";
 import ChampionImage from "../image/ChampionImage";
 import MasteryImage from "../image/MasteryImage";
 import styles from "./MasteryCard.scss";
 
 class MasteryCard extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {imageData: {}}
-  }
-
-  componentDidMount() {
-    this.fetchChampData(this.props);
-  }
-
-  fetchChampData(props) {
-
-    // TODO: Decide how to handle "static" champion data, then move this to the appropriate place.
-    ChampionData.get(props.championId).then(function(champion) {
-      this.setState({
-        imageData: champion.image
-      });
-    }.bind(this));
-  };
-
-  componentWillReceiveProps(nextProps) {
-    this.fetchChampData(nextProps);
-  }
 
   render() {
     return (
       <div>
         <div className={styles.wrapper}>
           <div className={styles.inner}>
-            <ChampionImage imageData={this.state.imageData} size="full"/>
+            <ChampionImage imageData={this.props.champion.image} size="full"/>
           </div>
         </div>
         <div className={styles.masteryIcon}>
